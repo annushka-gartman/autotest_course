ed3cead9ce3bcb5a4a4dc2621bedfebcc3502788
annushka.gartman <annushka.gartman@gmail.com>
2023-05-11 16:39:44 +0300

    Task 2 flatten and sort

---
+++ homework/task_4/task2.py
@@ -0,0 +1,23 @@
+def flatten_and_sort(array):
+    new_list = []
+    for list_data in array:
+        for number in list_data:
+            new_list.append(number)
+    result_list = sorted(new_list)
+    return result_list
+
+data = [
+    [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]],
+    [[], []],
+    [[], [1]],
+    [[1, 3, 5], [100], [2, 4, 6]]
+]
+
+test_data = [
+    [1, 2, 3, 4, 5, 6, 7, 8, 9], [], [1], [1, 2, 3, 4, 5, 6, 100]
+]
+
+for i, d in enumerate(data):
+    assert flatten_and_sort(d) == test_data[i], f'С набором {d} есть ошибка, не проходит проверку'
+    print(f'Тестовый набор {d} прошёл проверку')
+print('Всё ок')
